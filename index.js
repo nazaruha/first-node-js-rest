11987006bd1f1f3f8c7f3b0da83db4b1e7539125
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-11-03 13:46:55 +0100

    make simple api request

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json()); // tells that we gonna use json data in our app
+
+app.get("/", (req, res) => {
+    console.log("[TEST]!");
+
+    res.send('Hello from Homepage.');
+})
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));

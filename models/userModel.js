54a080880504e637ebb004d6fda824b2f9aab2fd
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-11-08 23:36:03 +0100

    create userModel

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+    firstName: {
+        type: String,
+        required: [true, "Please, enter a user's first name"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Please, enter a user's last name"]
+    },
+    age: {
+        type: Number,
+        require: [true, "Please, enter a user's age"],
+        default: 0
+    }
+}, { timestamps: true /* auto assign values to this properties*/});
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
